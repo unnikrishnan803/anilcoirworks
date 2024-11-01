df7077ed9d996e818aed3d6255abc402a964cf81
unnikrishnan803 <unnikrishnana803@gmail.com>
2024-11-01 10:31:52 +0530

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // script.js
-function redirectToWhatsApp(productName, price) {
-    const message = `Hello, I'm interested in buying ${productName} priced at ₹${price}. Could you provide more details?`;
-    const phoneNumber = '916235279256';
-    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+function redirectToWhatsApp() {
+    const url = 'https://wa.link/niqcxy';
     window.open(url, '_blank');
-  }
-  
+}
